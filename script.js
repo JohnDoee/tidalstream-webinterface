01fa6db581dec46cb939aefdb6b985cfbfc5226e
JohnDoee <johndoee@tidalstream.org>
2014-04-02 22:12:15 +0200

    hotfix

--- script.js
+++ script.js
@@ -699,6 +699,7 @@
               if (obj.playbackOutput.type === 'player' && obj.playbackOutput.obj.player_id === player_id || defaultPlayer && defaultPlayer.type === 'player' && defaultPlayer.playerId === player_id) {
                 obj.playbackOutput.obj = obj.players[player_id];
                 obj.playbackOutput.status = 'online';
+                obj.playbackOutput.type = 'player';
               }
               break;
             case 'update':
